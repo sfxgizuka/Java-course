cfffa89ba7b64e717a6c9525561de9db72cd8cf4
Gregory Izuka <gizuka@seamfix.com>
2023-11-22 11:17:43 +0100

    maps

---
+++ src/advanced/finalpractise/HangmanApp.java
@@ -0,0 +1,14 @@
+package advanced.finalpractise;
+
+import java.io.FileNotFoundException;
+
+public class HangmanApp {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        GameService gameService = new GameService();
+        gameService.start();
+
+
+    }
+
+}
